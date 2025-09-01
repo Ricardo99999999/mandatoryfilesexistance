ab9a3802e2a8fa3c129d438ada1adfa836e994c6
Osiris <ricardomartin9922@gmail.com>
2025-09-01 20:39:01 +0200

    Upload new repository mandatoryfilesexistance

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+
+// Always rely on the internal file of your package
+const filesListPath = path.join(__dirname, 'filesExistance.txt');
+
+if (!fs.existsSync(filesListPath)) {
+  console.error(`File not found ${filesListPath}`);
+  process.exit(1);
+}
+
+// Load and clean the list. Works in sigle files and folders
+const requiredFiles = fs.readFileSync(filesListPath, 'utf8')
+  .split('\n')                // Split into lines
+  .map(line => line.trim())   // Trim spaces
+  .filter(line => line !== '' && !line.startsWith('#')); // Ignore empty lines or comments
+
+let missing = [];
+
+for (const file of requiredFiles) {
+  if (!fs.existsSync(file)) {
+    missing.push(file);
+  }
+}
+
+if (missing.length > 0) {
+  console.error("Mandatory files are missing:", missing.join(', '));
+  process.exit(1); // Program or pipeline fails
+} else {
+  console.log("All mandatory files are present");
+  process.exit(0);
+}
